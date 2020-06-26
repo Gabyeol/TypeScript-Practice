ab57be771cf5e22e775b148a2afef524d343e071
Gabyeol <rainsection@naver.com>
2020-06-26 23:06:28 +0900

    Study about iterator

--- ch04/src/index.ts
+++ ch04/src/index.ts
@@ -44,3 +44,22 @@ function* gen12345() {
 
 for(let value of gen12345())
     console.log(value)
+
+
+const createRangeIterable = (from: number, to: number) => {
+    let currentValue = from
+    return {
+        next() {
+            const value = currentValue < to ? currentValue++ : undefined
+            const done = value == undefined
+            return {value, done}
+        }
+    }
+}
+
+const iterator2 = createRangeIterable(1, 3 + 1)
+while(true) {
+    const {value, done} = iterator2.next()
+    if(done) break
+    console.log(value)
+}
