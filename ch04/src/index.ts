e93205aff840cd088d7e8a1e311b04d4022f2d20
Gabyeol <rainsection@naver.com>
2020-06-28 20:44:27 +0900

    Study about function composition

--- ch04/src/index.ts
+++ ch04/src/index.ts
@@ -61,5 +61,18 @@ const iterator2 = createRangeIterable(1, 3 + 1)
 while(true) {
     const {value, done} = iterator2.next()
     if(done) break
-    console.log(value)
+    // console.log(value)
 }
+
+const f = <T>(x: T): string => `f(${x})`
+const g = <T>(x: T): string => `g(${x})`
+const h = <T>(x: T): string => `h(${x})`
+const compose = <T, R>(...functions: readonly Function[]): Function => (x:T): (T) => R => {
+    const deepCopiedFunctions = [...functions]
+    return deepCopiedFunctions.reverse().reduce((value, func) => func(value), x)
+}
+
+const composedFunction = compose(h, g, f)
+console.log(
+    composedFunction('x')
+)
